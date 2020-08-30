b59f9f8e4c4341f3749d72f8408c026ba53008ed
RJS66 <rjs.github.com>
2020-08-30 16:09:37 -0400

    finish malloc, need to add documentation for clarity

--- mymalloc.c
+++ mymalloc.c
@@ -1,24 +1,79 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
-struct header_t {
-   size_t size;
-   unsigned is_free;
-   struct header_t *next;
-}
+typedef char ALIGN[16];
 
-void *malloc(size_t size);
+union header {
+   struct {
+         size_t size;
+         unsigned is_free;
+         union header *next;
+   } s;
+   /* force the header to be aligned to 16 bytes */
+   ALIGN stub;
+};
+
+typedef union header header_t;
+
+header_t *head, *tail;
+pthread_mutex_t global_malloc_lock;
+header_t *get_free_block(size_t size);
 
 int main(int argc, char *argv[]) {
 
+   union header test;
+   printf("header:\t%lu\n", sizeof(test));
 
 
    return 0;
 }
 
 void *malloc(size_t size) {
+
+   size_t total_size;
    void *block;
-   block = sbrk(size);
-   if (block == (void*) - 1)
+   header_t *header;
+   if (!size)
       return NULL;
-   return block;
+   
+   pthread_mutex_lock(&global_malloc_lock);
+   header = get_free_block(size);
+   if (header) {
+      header->s.is_free = 0;
+      pthread_mutex_unlock(&global_malloc_lock);
+      return (void*)(header + 1);
+   }
+
+   total_size = sizeof(header_t) + size;
+   block = sbrk(total_size);
+   if (block == (void*) -1) {
+      pthread_mutex_unlock(&global_malloc_lock);
+      return NULL;
+   }
+   header = block;
+   header->s.size = size;
+   header->s.is_free = 0;
+   header->s.next = NULL;
+   
+   if (!head)
+      head = header;
+   if (tail)
+      tail->s.next = header;
+   tail = header;
+   pthread_mutex_unlock(&global_malloc_lock);
+   return (void*)(header + 1);
+
+}
+
+header_t *get_free_block(size_t size) {
+   
+   header_t *curr = head;
+   while (curr) {
+      if (curr->s.is_free && curr->s.size >= size)
+         return curr;
+      curr = curr->s.next;
+   }
+   return NULL;
+
 }
